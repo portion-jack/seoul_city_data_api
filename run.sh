45aef96ba9be2d58baac55b745cb70f6f7bbb1e2
portion-jack <flycast0080@gmail.com>
2022-11-04 05:33:42 +0000

    2022-11-04

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if (source seoul_api/bin/activate) ; then
     python _seoul_api.py
     
     git add .
-    git commit -m "`date + '%Y-%m-%d'`"
+    git commit -m "`date '+%Y-%m-%d'`"
     git push origin main
 else
     echo "conda should be activated"
